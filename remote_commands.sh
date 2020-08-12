912288128e39023a1987884edf3c9ddac209e613
Ubuntu <ubuntu@ip-172-31-19-20.us-west-2.compute.internal>
2020-08-12 14:16:12 +0000

    run

--- remote_commands.sh
+++ remote_commands.sh
@@ -1,3 +1,5 @@
+
+
 dokku apps:create hbf
 
 
@@ -5,3 +7,20 @@ dokku apps:create hbf
 dokku postgres:create hbf
 
 dokku postgres:link hbf hbf
+
+
+# set airflow database variable
+dokku config:set hbf AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgres://postgres:72325126e072a2b62a52613b27813a79@dokku-postgres-hbf:5432/hbf"
+
+dokku config:set hbf AIRFLOW__CORE__LOAD_EXAMPLES=False
+
+dokku config:set hbf AIRFLOW_HOME=/app
+
+dokku config:set hbf AIRFLOW__CORE__FERNET_KEY="iEyZLCe5fOC1bMg-jVwC9o96mHPUxzMWlsboeqOyS3Q="
+
+echo "web: airflow initdb" > Procfile
+
+# git add git push
+
+echo "web: airflow webserver --port $PORT" > Procfile
+
