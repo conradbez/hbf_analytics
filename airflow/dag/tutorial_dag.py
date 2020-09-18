d016e44ecc7e0af2a8f119f83059c8caf4c32dce
conradbez <conradbez1@gmail.com>
2020-09-18 06:48:12 +0000

    f

--- airflow/dag/tutorial_dag.py
+++ airflow/dag/tutorial_dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import dagfactory
 
-dag_factory = dagfactory.DagFactory("./tutorial_file.yml")
+dag_factory = dagfactory.DagFactory("./tutorial_dag.yml")
 
 dag_factory.clean_dags(globals())
 dag_factory.generate_dags(globals())
