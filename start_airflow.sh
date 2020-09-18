6825faae5a840d7cfdb1162ec0ed638c20ea5fae
conradbez <conradbez1@gmail.com>
2020-09-18 07:52:55 +0000

    f

--- start_airflow.sh
+++ start_airflow.sh
@@ -3,6 +3,6 @@ export AIRFLOW_VAR_HOME=$(pwd)/airflow
 export PORT=8080
 airflow initdb
 sleep 20
-airflow webserver -p $PORT -D
+airflow scheduler &
 sleep 5
-airflow scheduler -D
+airflow webserver -p $PORT
