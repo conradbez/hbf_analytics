5495ac14977ca83225c3d8bc3a9ef58ea1e1c980
conradbez <conradbez1@gmail.com>
2020-09-18 07:13:28 +0000

    f

--- airflow/dags/tutorial_dag.py
+++ airflow/dags/tutorial_dag.py
@@ -2,7 +2,6 @@ from airflow import DAG
 import dagfactory
 from airflow.models import Variable
 airflow_home_path = Variable.get("home")
-foo_json = Variable.get("foo_baz", deserialize_json=True)
 
 dag_factory = dagfactory.DagFactory(airflow_home_path+"/dags/tutorial_dag.yml")
 
---
+++ start_airflow.sh
@@ -0,0 +1,9 @@
+export AIRFLOW_HOME=$(pwd)/airflow
+export AIRFLOW_VAR_HOME=$(pwd)/airflow
+export PORT=8080
+airflow initdb
+sleep 20
+airflow webserver -p $PORT -D
+export AIRFLOW_HOME=$(pwd)/airflow  
+sleep 5
+airflow scheduler -D
