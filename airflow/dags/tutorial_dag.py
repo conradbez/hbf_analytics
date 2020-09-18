56ff37aae63e375f2b041a713d331d8511e36762
conradbez <conradbez1@gmail.com>
2020-09-18 07:06:23 +0000

    f

--- airflow/dags/tutorial_dag.py
+++ airflow/dags/tutorial_dag.py
@@ -1,7 +1,10 @@
 from airflow import DAG
 import dagfactory
+from airflow.models import Variable
+airflow_home_path = Variable.get("home")
+foo_json = Variable.get("foo_baz", deserialize_json=True)
 
-dag_factory = dagfactory.DagFactory("./tutorial_dag.yml")
+dag_factory = dagfactory.DagFactory(airflow_home_path+"/dags/tutorial_dag.yml")
 
 dag_factory.clean_dags(globals())
 dag_factory.generate_dags(globals())
